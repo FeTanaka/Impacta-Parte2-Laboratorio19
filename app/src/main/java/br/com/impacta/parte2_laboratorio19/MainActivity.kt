ce924357e8b90f8f3495e562494e99adb36692c3
curso <curso@curso.com>
2022-07-27 22:24:35 -0300

    commit inicial + layouts + gráfico de navegação

---
+++ app/src/main/java/br/com/impacta/parte2_laboratorio19/MainActivity.kt
@@ -0,0 +1,11 @@
+package br.com.impacta.parte2_laboratorio19
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
